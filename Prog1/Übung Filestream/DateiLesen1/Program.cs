0f708243cf6edc531ca9212d31c55a00ffe733c0
david <87530266+davidg-h@users.noreply.github.com>
2021-07-16 15:50:53 +0200

    Add files via upload

---
+++ Prog1/Übung Filestream/DateiLesen1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace DateiLesen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int countmax = 0;
+            int countmax2 = 0;
+            int countTiefe = 0;
+            int maxCountTiefe = 0;
+            bool structure = true;
+
+            string path = @"C:\Users\David\Desktop\Schule\Prog1 Projekte\Übungen\DateiLesen\Program.cs";
+            FileStream s = new FileStream(path, FileMode.Open);
+
+            StreamReader r = new StreamReader(s);
+
+            string line = r.ReadLine();
+            while (line != null)
+            {
+                foreach (char c in line)
+                {
+                    if (c == '{')
+                    {
+                        count++;
+                        countmax++;
+                        countTiefe++;
+                        if (countTiefe > maxCountTiefe)
+                        {
+                            maxCountTiefe = countTiefe;
+                        }
+                    }
+                    else if (c == '}')
+                    {
+                        count--;
+                        countmax2++;
+                        countTiefe--;
+                        if (count < 0)
+                        {
+                            structure = false;
+                        }
+                    }
+                }
+                line = r.ReadLine();
+            }
+            Console.WriteLine($"Der Code ist richtig strukturiert: {structure} Count muss 0 sein: {count}");
+            Console.WriteLine($"\nCount für offene Klammmern {countmax}");
+            Console.WriteLine($"\nCount für geschlossene Klammmern {countmax2}");
+            Console.WriteLine($"\nDie tiefste Verschachtelung ist {maxCountTiefe}");
+
+            r.Close();
+        }
+    }
+}
---
+++ Prog1/Übung Filestream/WetterVerarbeiten/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace WetterVerarbeiten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] durchschnitte = Monatswerte("München", 2010);
+            double[] durchschnitte2 = Monatswerte("Nürnberg", 2010);
+            double[] durchschnitte3 = Monatswerte("Würzburg", 2010);
+            for (int i = 0; i < durchschnitte.Length; i++)
+            {
+                Console.WriteLine($"Monat {i + 1}: {durchschnitte[i]:f2} °C");
+                Console.WriteLine($"Monat {i + 1}: {durchschnitte2[i]:f2} °C");
+                Console.WriteLine($"Monat {i + 1}: {durchschnitte3[i]:f2} °C");
+            }
+
+        }
+
+        public static double[] Monatswerte(string stadt, int jahr)
+        {
+            string path = @"C:\Users\David\Desktop\Schule\Prog1 Projekte\Übungen\DateiLesen\WetterVerarbeiten\wetter.txt";
+            FileStream stream = new FileStream(path, FileMode.Open);
+            StreamReader r = new StreamReader(stream);
+            string line = r.ReadLine();
+
+            double[] middle = new double[12];
+            double[] counter = new double[12];
+
+            while (line != null)
+            {
+                string[] neuLine = line.Split("\t");
+                string[] date = neuLine[1].Split("-");
+                if (neuLine[0] == stadt && date[0] == jahr.ToString())
+                {
+                    middle[Convert.ToInt32(date[1]) - 1] += Convert.ToDouble(neuLine[3]);
+                    counter[Convert.ToInt32(date[1]) - 1]++;
+                }
+                line = r.ReadLine();
+            }
+
+            for (int i = 0; i < middle.Length; i++)
+            {
+                middle[i] = middle[i] / counter[i];
+            }
+
+            r.Close();
+            return middle;
+        }
+    }
+}
